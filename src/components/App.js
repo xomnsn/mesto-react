2386208586b80f3d5cf9d5aaf69269797e8370f7
Mariya Murasheva <xomnsn@gmail.com>
2021-04-07 16:12:33 +0300

    hotfix: popup children as props

--- src/components/App.js
+++ src/components/App.js
@@ -48,6 +48,22 @@ function App() {
       <PopupWithForm
         name="edit-user"
         title="Редактировать профиль"
+        children={(
+          <>
+            <input type="text" name="name"
+              required minLength="2" maxLength="40"
+              placeholder="Имя"
+              className="popup__text-input popup__text-input_type_name"
+              id="user-name"/>
+            <p className="popup__input-error" id="user-name-error">Ошибка</p>
+            <input type="text" name="about"
+              required minLength="2" maxLength="200"
+              placeholder="Описание"
+              className="popup__text-input popup__text-input_type_description"
+              id="user-description"/>
+            <p className="popup__input-error" id="user-description-error">Ошибка</p>
+          </>
+        )}
         button="Сохранить"
         isOpen={isEditUserPopupOpen}
         onClose={closeAllPopups}
@@ -55,6 +71,16 @@ function App() {
       <PopupWithForm
         name="edit-avatar"
         title="Обновить аватар"
+        children={(
+          <>
+            <input type="url" name="link" value=""
+              required
+              placeholder="Ссылка на картинку"
+              className="popup__text-input popup__text-input_type_avatar-src"
+              id="avatar-url" />
+            <p className="popup__input-error" id="avatar-url-error"/>
+          </>
+        )}
         button="Сохранить"
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
@@ -62,6 +88,18 @@ function App() {
       <PopupWithForm
         name="add-place"
         title="Новое место"
+        children={(
+          <>
+            <input type="text" name="name" value=""
+              required minLength="2" maxLength="30"
+              placeholder="Название"
+              className="popup__text-input popup__text-input_type_title" id="card-title" />
+            <p className="popup__input-error" id="card-title-error"/>
+            <input type="url" name="link" value="" required placeholder="Ссылка на картинку"
+                     className="popup__text-input popup__text-input_type_src" id="card-url" />
+            <p className="popup__input-error" id="card-url-error"/>
+          </>
+        )}
         button="Создать"
         isOpen={isAddPlacePopupOpen}
         onClose={closeAllPopups}
@@ -71,6 +109,7 @@ function App() {
         title="Вы уверены?"
         button="Да"
         isOpen={false}
+        children={(<></>)}
         onClose={closeAllPopups}
       />
       <ImagePopup
